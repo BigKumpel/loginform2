62641cf33060b6bc0f64e227df71a59d4ea668bb
BigKumpel <sockokacper@gmail.com>
2022-11-30 10:57:17 +0100

    route

--- class/userclass.php
+++ class/userclass.php
@@ -14,5 +14,47 @@ class User {
         $this->lastName = "";
         global $db;
         $this->db = &$db;
-    }    
+    }
+    
+    public function register() : bool {
+        $passwordHash = password_hash($this->password, PASSWORD_ARGON2I);
+        $query = "INSERT INTO user VALUES (NULL, ?, ?, ?, ?)";
+        $preparedQuery = $this->db->prepare($query); 
+        $preparedQuery->bind_param('ssss', $this->login, $passwordHash, 
+                                            $this->firstName, $this->lastName);
+        $result = $preparedQuery->execute();
+        return $result;
+    }
+
+    public function login() : bool {
+        $query = "SELECT * FROM user WHERE login = ? LIMIT 1";
+        $preparedQuery = $this->db->prepare($query); 
+        $preparedQuery->bind_param('s', $this->login);
+        $preparedQuery->execute();
+        $result = $preparedQuery->get_result();
+        if($result->num_rows == 1) {
+            $row = $result->fetch_assoc();
+            $passwordHash = $row['password'];
+            if(password_verify($this->password, $passwordHash)) {
+                $this->id = $row['id'];
+                $this->firstName = $row['firstName'];
+                $this->lastName = $row['lastName'];
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+    public function setFirstName(string $firstName) {
+        $this->firstName = $firstName;
+    }
+    public function setLastName(string $lastName) {
+        $this->lastName = $lastName;
+    }
+    public function getName() : string {
+        return $this->firstName . " " . $this->lastName;
+    }
+}
 ?>
--- index.php
+++ index.php
@@ -1,5 +1,17 @@
 <?php
-require_once('vendor/autoload.php');
+
+use Steampixel\Route;
+
 require_once('config.php');
 require_once('class/userclass.php');
+
+Route::add('/', function() {
+    echo "strona";
+});
+
+Route::add('/login', function() {
+    echo "strona logowania";
+});
+
+Route::run('/loginform');
 ?>
