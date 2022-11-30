ccf88ab054ec2fab4ff9ec6074fbc9f78623df62
BigKumpel <sockokacper@gmail.com>
2022-11-30 12:31:06 +0100

    session

--- index.php
+++ index.php
@@ -1,13 +1,44 @@
 <?php
-
+session_start();
 use Steampixel\Route;
 
 require_once('config.php');
 require_once('class/userclass.php');
 
 Route::add('/', function() {
-    echo "strona";
+    global $twig;
+    $v = array(
+        'authorized' => $_SESSION['authorized'],
+        'user' => $_SESSION['user'], 
+    );
+    $twig->display('home.html.twig', ['authorized' => $_SESSION['authorized']]);
 });
+
+Route::add('/login', function() {
+    global $twig;
+    $twig->display('login.html.twig');
+});
+
+Route::add('/login', function() {
+    global $twig;
+    if(isset($_REQUEST['login']) && isset($_REQUEST['password'])) {
+        
+        $user = new User($_REQUEST['login'], $_REQUEST['password']);
+        if($user->login()) { 
+            $_SESSION['authorized'] = true;
+            $_SESSION['user'] = $user;   
+            $v = array(
+                'message' => "Zalogowano poprawnie użytkownika: ".$user->getName(),
+                'authorized' => $_SESSION['authorized'],
+            );
+            $twig->display('message.html.twig', $v);
+        } else {
+            $twig->display('register.html.twig', ['message' => "Błędny login lub hasło"]);
+        }
+    } else {
+        $twig->display('register.html.twig');
+    }
+}, 'post');
 
 Route::add('/login', function() {
     echo "strona logowania";
--- login.php
+++ login.php
@@ -1,16 +1,17 @@
 <?php
 if(isset($_REQUEST['login']) && isset($_REQUEST['password'])) {
-    require_once('config.php');
-    require_once('class/User.class.php');
+    //jeżeli już podano dane do logowania
+    
     $user = new User($_REQUEST['login'], $_REQUEST['password']);
     if($user->login()) {
-        echo "Zalogowano poprawnie użytkownika: ".$user->getName();
+        $v = array(
+            'message' => "Zalogowano poprawnie użytkownika: ".$user->getName(),
+        );
+        $twig->display('message.html.twig', $v);
     } else {
-        echo "Błędny login lub hasło";
-    } else {
-        $twig->display("message.html.twig", ['message'=> "Błędny login lub hasło"]);
+        $twig->display('message.html.twig', 
+                            ['message' => "Błędny login lub hasło"]);
     }
 } else {
-    $twig->display("login.html.twig");
-}
+    $twig->display('login.html.twig');
 ?> 
